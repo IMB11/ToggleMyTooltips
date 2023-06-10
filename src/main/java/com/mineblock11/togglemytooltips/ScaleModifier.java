8061c520c5a437d6752758595204c22ba1f1494f
Calum <contact@mineblock11.dev>
2023-06-10 20:10:40 +0100

    1.20

--- src/main/java/com/mineblock11/togglemytooltips/ScaleModifier.java
+++ src/main/java/com/mineblock11/togglemytooltips/ScaleModifier.java
@@ -1,18 +1,19 @@
 package com.mineblock11.togglemytooltips;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public enum ScaleModifier {
-    SMALL((args) -> {
-        MatrixStack matrices = args.get(0);
+    SMALL((args, matrices) -> {
         Text text = args.get(1);
-        float x = args.get(2);
-        float y = args.get(3);
+        int x = args.get(2);
+        int y = args.get(3);
 
         float scaleFactor = 0.52f;
 
@@ -27,11 +28,11 @@ public enum ScaleModifier {
 //        args.set(2, 1f);
 //        args.set(3, 1f);
 
-        args.set(2, scaleOffsetX - (scaleTextWidth * 1.52f));
-        args.set(3, scaleOffsetY + (scaleTextHeight * 1.52f));
+        args.set(2, (int) (scaleOffsetX - (scaleTextWidth * 1.52f)));
+        args.set(3, (int) (scaleOffsetY + (scaleTextHeight * 1.52f)));
     }),
-    NORMAL((args) -> {
-        float x = args.get(2);
+    NORMAL((args, matrices) -> {
+        int x = args.get(2);
         Text text = args.get(1);
         float scaleTextWidth = (MinecraftClient.getInstance().textRenderer.getWidth(text));
         x *= 2f;
@@ -39,36 +40,31 @@ public enum ScaleModifier {
         x /= 2f;
         args.set(2, x);
     }),
-    LARGE((args) -> {
-        MatrixStack matrices = args.get(0);
+    LARGE((args, matrices) -> {
         Text text = args.get(1);
-        float x = args.get(2);
-        float y = args.get(3);
+        int x = args.get(2);
+        int y = args.get(3);
 
         float scaleFactor = 2f;
 
         float scaleTextWidth = (MinecraftClient.getInstance().textRenderer.getWidth(text));
-        float scaleTextHeight = (MinecraftClient.getInstance().textRenderer.fontHeight);
 
         float scaleOffsetX = (x) / scaleFactor;
         float scaleOffsetY = (y) / scaleFactor;
 
         matrices.scale(scaleFactor, scaleFactor, scaleFactor);
 
-//        args.set(2, 1f);
-//        args.set(3, 1f);
-
-        args.set(2, scaleOffsetX - (scaleTextWidth / 2f));
-        args.set(3, scaleOffsetY);
+        args.set(2, (int) (scaleOffsetX - (scaleTextWidth / 2f)));
+        args.set(3, (int) scaleOffsetY);
     });
 
-    public Consumer<Args> getMixinEditFunction() {
+    public BiConsumer<Args, MatrixStack> getMixinEditFunction() {
         return mixinEditFunction;
     }
 
-    private final Consumer<Args> mixinEditFunction;
+    private final BiConsumer<Args, MatrixStack> mixinEditFunction;
 
-    ScaleModifier(Consumer<Args> mixinEditFunction) {
+    ScaleModifier(BiConsumer<Args, MatrixStack> mixinEditFunction) {
         this.mixinEditFunction = mixinEditFunction;
     }
 }
--- src/main/java/com/mineblock11/togglemytooltips/mixin/InGameHudMixin.java
+++ src/main/java/com/mineblock11/togglemytooltips/mixin/InGameHudMixin.java
@@ -2,6 +2,7 @@ package com.mineblock11.togglemytooltips.mixin;
 
 import com.mineblock11.togglemytooltips.ScaleModifier;
 import com.mineblock11.togglemytooltips.ToggleMyTooltips;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.MutableText;
@@ -11,6 +12,7 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
@@ -25,27 +27,29 @@ public class InGameHudMixin {
     @Shadow @Nullable private Text overlayMessage;
 
     @Shadow private int scaledWidth;
+    @Unique private DrawContext tempContext;
 
-    @Inject(method = "renderHeldItemTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;drawWithShadow(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;FFI)I"))
-    public void preScale(MatrixStack matrices, CallbackInfo ci) {
-        matrices.push();
+    @Inject(method = "renderHeldItemTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)I", shift = At.Shift.BEFORE))
+    public void preScale(DrawContext context, CallbackInfo ci) {
+        context.getMatrices().push();
+        tempContext = context;
     }
 
-    @Inject(method = "renderHeldItemTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;drawWithShadow(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;FFI)I", shift = At.Shift.AFTER))
-    public void postScale(MatrixStack matrices, CallbackInfo ci) {
-        matrices.pop();
+    @Inject(method = "renderHeldItemTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)I", shift = At.Shift.AFTER))
+    public void postScale(DrawContext context, CallbackInfo ci) {
+        context.getMatrices().pop();
     }
 
-    @ModifyArgs(method = "renderHeldItemTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;drawWithShadow(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;FFI)I"))
+    @ModifyArgs(method = "renderHeldItemTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)I"))
     public void modifyArgsHudOverlayScale(Args args) {
-        args.set(2, (this.scaledWidth) / 2f);
+        args.set(2, (int) (this.scaledWidth / 2f));
 
         ScaleModifier modifier = ToggleMyTooltips.CONFIG.scaleModifier();
         boolean enabled = ToggleMyTooltips.CONFIG.showHeldItemTooltip();
         boolean shouldApplyFormatting = ToggleMyTooltips.CONFIG.shouldApplyFormattingExtras();
         List<String> formattingsPre = ToggleMyTooltips.CONFIG.formattingExtras();
 
-        modifier.getMixinEditFunction().accept(args);
+        modifier.getMixinEditFunction().accept(args, tempContext.getMatrices());
 
         if(!enabled) {
             args.set(1, Text.empty());
